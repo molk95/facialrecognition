8527334ffa8b0105b8934ae0bb3c2712ca622960
molk95 <molk.saouabi@gmail.com>
2021-06-10 11:36:06 +0200

    start

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navigation = () => {
+  <nav style={{display: 'flax', justifyContent: 'center'}} >
+    <p>Sign out</p>
+  </nav>;
+};
+
+export default Navigation;
