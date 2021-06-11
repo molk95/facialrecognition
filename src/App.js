2673a26306cf9a229e7593d1d94114c5a5bd8cca
molk95 <molk.saouabi@gmail.com>
2021-06-11 16:14:48 +0100

    particles.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import React from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 
 import './App.css';
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3ca9d1",
+        blur: 5
+      }
+    }
+  }
+}
 
-function App() {
-  return (
-    <div className="App">
-     <Navigation />
-      <Logo />
-      <Rank />
-     <ImageLinkForm />
-    {/* <FaceRecognition /> */}
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+         <Particles className='particles '
+         params={particlesOptions} /> 
+         <Navigation />
+         <Logo />
+         <Rank />
+         <ImageLinkForm />
+      {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Rank.css'
 
 const Rank = () => {
     return (
         <div>
-            
+            <div className='ranker_name'>
+                {'Molk your current rank is...'}
+            </div>
+            <div className='ranker_number'>
+                {'#5'}
+            </div>
         </div>
     )
 }
