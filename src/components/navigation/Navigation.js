06517cccbbdd26c5cece4f32d12ca32c60c16907
molk95 <molk.saouabi@gmail.com>
2021-06-13 14:46:26 +0100

    nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "../navigation/Navigation.css"
 
-const Navigation = ({onRouteChange}) => {
-  return (
+const Navigation = ({onRouteChange, isSignedIn}) => {
+  
+    if (isSignedIn) {
+      return (
+        <nav >
+       <span onClick={() => onRouteChange('signin')} className='login'> Sign out</span>
+        </nav>
 
-  <nav >
-    <span onClick={() => onRouteChange('signin')} className='login'> Sign out</span>
-    
-  </nav>
-  )
+      );
+       
+ } else {
+   return (
+    <nav >
+    <span onClick={() => onRouteChange('signin')} className='login'> Sign in</span>
+    <span onClick={() => onRouteChange('register')} className='login'> Sign up</span>
+     </nav>
+   );
+ }
 };
 
 export default Navigation;
